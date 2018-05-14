e51c7bebbd9776aeb308b9d5ae08346481fdaef1
scanner-darkly <scannerdarkly.git@gmail.com>
2018-05-13 19:17:44 -0700

    undo for script editing

--- module/edit_mode.c
+++ module/edit_mode.c
@@ -18,11 +18,18 @@
 #include "conf_usb_host.h"  // needed in order to include "usb_protocol_hid.h"
 #include "usb_protocol_hid.h"
 
+#define UNDO_DEPTH 3
+
 static line_editor_t le;
-static uint8_t line_no1 = 0, line_no2 = 0;
+static uint8_t line_no1, line_no2;
 static uint8_t script;
 static error_t status;
 static char error_msg[TELE_ERROR_MSG_LENGTH];
+static tele_command_t undo_buffer[UNDO_DEPTH][SCRIPT_MAX_COMMANDS];
+static uint8_t undo_comments[UNDO_DEPTH][SCRIPT_MAX_COMMANDS];
+static uint8_t undo_length[UNDO_DEPTH];
+static uint8_t undo_line_no1[UNDO_DEPTH], undo_line_no2[UNDO_DEPTH];
+static uint8_t undo_count, undo_pos;
 
 static const uint8_t D_INPUT = 1 << 0;
 static const uint8_t D_LIST = 1 << 1;
@@ -36,12 +43,50 @@ void set_edit_mode() {
     line_no2 = line_no1 = 0;
     line_editor_set_command(
         &le, ss_get_script_command(&scene_state, script, line_no1));
+    undo_count = 0;
     dirty = D_ALL;
 }
 
 void set_edit_mode_script(uint8_t new_script) {
     script = new_script;
     if (script >= SCRIPT_COUNT) script = SCRIPT_COUNT - 2;
+    undo_count = 0;
+    dirty = D_ALL;
+}
+
+static void save_undo(void) {
+    if (++undo_count > 3) undo_count = 3;
+    undo_pos = (undo_pos + 1) % UNDO_DEPTH;
+    undo_line_no1[undo_pos] = line_no1;
+    undo_line_no2[undo_pos] = line_no2;
+    undo_length[undo_pos] = ss_get_script_len(&scene_state, script);
+    for (u8 l = 0; l < undo_length[undo_pos]; l++) {
+        undo_comments[undo_pos][l] = ss_get_script_comment(&scene_state,
+            script, l);
+        ss_copy_script_command(&undo_buffer[undo_pos][l], 
+            &scene_state, script, l);
+    }
+}
+
+static void undo(void) {
+    if (undo_count == 0) return;
+    undo_count--;
+    
+    ss_clear_script(&scene_state, script);
+    for (u8 l = 0; l < undo_length[undo_pos]; l++) {
+        ss_insert_script_command(&scene_state, script, l,
+            &undo_buffer[undo_pos][l]);
+        ss_set_script_comment(&scene_state, script, l,
+            undo_comments[undo_pos][l]);
+    }
+    
+    line_no1 = undo_line_no1[undo_pos];
+    line_no2 = undo_line_no2[undo_pos];
+
+    undo_pos = (undo_pos + UNDO_DEPTH - 1) % UNDO_DEPTH;
+
+    line_editor_set_command(
+        &le, ss_get_script_command(&scene_state, script, line_no1));
     dirty = D_ALL;
 }
 
@@ -50,8 +95,12 @@ void edit_mode_refresh() {
 }
 
 void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
+    // C-z: undo
+    if (match_ctrl(m, k, HID_Z)) {
+        undo();
+    }
     // <down> or C-n: line down
-    if (match_no_mod(m, k, HID_DOWN) || match_ctrl(m, k, HID_N)) {
+    else if (match_no_mod(m, k, HID_DOWN) || match_ctrl(m, k, HID_N)) {
         if (line_no1 < (SCRIPT_MAX_COMMANDS - 1) &&
             line_no1 < ss_get_script_len(&scene_state, script)) {
             line_no1++;
@@ -116,6 +165,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
             &le, ss_get_script_command(&scene_state, script, line_no1));
         line_no2 = line_no1;
         dirty |= D_LIST | D_INPUT;
+        undo_count = 0;
     }
     // ]: next script
     else if (match_no_mod(m, k, HID_CLOSE_BRACKET)) {
@@ -129,6 +179,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
             &le, ss_get_script_command(&scene_state, script, line_no1));
         line_no2 = line_no1;
         dirty |= D_LIST | D_INPUT;
+        undo_count = 0;
     }
     // alt-<down>: move selected lines down
     else if (match_alt(m, k, HID_DOWN)) {
@@ -136,6 +187,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
         u8 l2 = max(line_no1, line_no2);
         if (l2 < SCRIPT_MAX_COMMANDS - 1 &&
             l2 < max(1, ss_get_script_len(&scene_state, script)) - 1) {
+            save_undo();
             tele_command_t temp;
             ss_copy_script_command(&temp, &scene_state, script, l2 + 1);
             ss_delete_script_command(&scene_state, script, l2 + 1);
@@ -150,6 +202,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
         u8 l1 = min(line_no1, line_no2);
         u8 l2 = max(line_no1, line_no2);
         if (l1 && l2 < ss_get_script_len(&scene_state, script)) {
+            save_undo();
             tele_command_t temp;
             ss_copy_script_command(&temp, &scene_state, script, l1 - 1);
             ss_delete_script_command(&scene_state, script, l1 - 1);
@@ -163,12 +216,14 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
     else if (match_ctrl(m, k, HID_X) || match_alt(m, k, HID_X)) {
         if (line_no1 == line_no2) {
             if (line_no1 < ss_get_script_len(&scene_state, script)) {
+                save_undo();
                 strcpy(copy_buffer[0], line_editor_get(&le));
                 copy_buffer_len = 1;
                  ss_delete_script_command(&scene_state, script, line_no1);
             }
         }
         else {
+            save_undo();
             u8 l1 = min(line_no1, line_no2);
             u8 l2 = max(line_no1, line_no2);
             copy_buffer_len = 0;
@@ -196,6 +251,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
             if (processed) dirty |= D_INPUT;
         }
         else {
+            save_undo();
             copy_buffer_len = 0;
             for (u8 l = min(line_no1, line_no2); l <= max(line_no1, line_no2); l++)
                 print_command(ss_get_script_command(&scene_state, script, l),
@@ -206,6 +262,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
     else if (match_ctrl(m, k, HID_V) || match_alt(m, k, HID_V)) {
         if (copy_buffer_len == 0) return;
 
+        save_undo();
         u8 idx = min(line_no1, line_no2);
         line_no1 = idx;
         tele_command_t command;
@@ -229,6 +286,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
         u8 l1 = min(line_no1, line_no2);
         u8 l2 = max(line_no1, line_no2);
         if (l1 < ss_get_script_len(&scene_state, script)) {
+            save_undo();
             for (s8 l = l2; l >= l1; l--)
                 ss_delete_script_command(&scene_state, script, l);
             if (line_no1 > ss_get_script_len(&scene_state, script))
@@ -262,6 +320,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
         if (status != E_OK)
             return;  // quit, screen_refresh_edit will display the error message
 
+        save_undo();
         if (command.length == 0) {  // blank line, delete the command
             ss_delete_script_command(&scene_state, script, line_no1);
             if (line_no1 > ss_get_script_len(&scene_state, script)) {
@@ -295,6 +354,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
         if (status != E_OK)
             return;  // quit, screen_refresh_edit will display the error message
 
+        save_undo();
         if (command.length > 0) {
             ss_insert_script_command(&scene_state, script, line_no1, &command);
             if (line_no1 < (SCRIPT_MAX_COMMANDS - 1)) { line_no1++; }
@@ -308,6 +368,7 @@ void process_edit_keys(uint8_t k, uint8_t m, bool is_held_key) {
     // alt-slash comment toggle selected lines
     else if (match_alt(m, k, HID_SLASH)) {
         if (line_no1 >= ss_get_script_len(&scene_state, script)) return;
+        save_undo();
         for (u8 l = min(line_no1, line_no2); l <= max(line_no1, line_no2); l++)
             ss_toggle_script_comment(&scene_state, script, l);
         dirty |= D_LIST;
