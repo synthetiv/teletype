5df2ef46f087fefed5cbe7e4b9b743b00b988618
John Mitchell <jlmitch5@ncsu.edu>
2018-11-18 13:34:58 -0500

    fix pdf rendering regression and remove hash from doc headers

--- utils/docs.py
+++ utils/docs.py
@@ -19,8 +19,7 @@ DOCS_DIR = ROOT_DIR / "docs"
 OP_DOCS_DIR = DOCS_DIR / "ops"
 FONTS_DIR = ROOT_DIR / "utils" / "fonts"
 TT_VERSION = get_tt_version()
-VERSION_STR = " ".join(["Teletype", TT_VERSION["tag"], TT_VERSION["hash"],
-                        "Documentation"])
+VERSION_STR = " ".join(["Teletype", TT_VERSION["tag"], "Documentation"])
 
 env = jinja2.Environment(
     autoescape=False,
@@ -76,32 +75,38 @@ def common_md():
     print(f"Using ops docs directory: {OP_DOCS_DIR}")
     print()
 
-    op_list_template = env.get_template("op_list.jinja2.md")
-    op_table_template = env.get_template("op_table.jinja2.md")
+    html_op_list_template = env.get_template("html_op_list.jinja2.md")
+    html_op_table_template = env.get_template("html_op_table.jinja2.md")
+    pdf_op_table_template = env.get_template("pdf_op_table.jinja2.md")
+    pdf_op_extended_template = env.get_template("pdf_op_extended.jinja2.md")
 
-    output = ""
-    output += Path(DOCS_DIR / "intro.md") \
+    intro = ""
+    intro += Path(DOCS_DIR / "intro.md") \
         .read_text().replace("VERSION", TT_VERSION["tag"][1:]) + "\n\n"
-    output += Path(DOCS_DIR / "whats_new.md").read_text() + "\n\n"
-    output += Path(DOCS_DIR / "quickstart.md").read_text() + "\n\n"
-    output += Path(DOCS_DIR / "keys.md").read_text() + "\n\n"
-    output += Path(DOCS_DIR / "ops.md").read_text() + "\n\n"
+    intro += Path(DOCS_DIR / "whats_new.md").read_text() + "\n\n"
+    intro += Path(DOCS_DIR / "quickstart.md").read_text() + "\n\n"
+    intro += Path(DOCS_DIR / "keys.md").read_text() + "\n\n"
+    intro += Path(DOCS_DIR / "ops.md").read_text() + "\n\n"
 
     all_ops = set(list_ops()) | set(list_mods())
     all_ops_dict = {}
 
     ops_with_docs = set()
 
+    pdf_ops_section = ""
+    html_ops_section = ""
+
     for section in OPS_SECTIONS:
         md_file = Path(OP_DOCS_DIR, section + ".md")
         toml_file = Path(OP_DOCS_DIR, section + ".toml")
 
-        output += "\\newpage\n"
+        pdf_ops_section += "\\newpage\n"
+        html_ops_section += "\\newpage\n"
 
         if md_file.exists() and md_file.is_file():
             print(f"Reading {md_file}")
-            output += md_file.read_text() + "\n\n"
-            output += "\n"
+            pdf_ops_section += md_file.read_text() + "\n\n"
+            html_ops_section += md_file.read_text() + "\n\n"
 
         if toml_file.exists() and toml_file.is_file():
             print(f"Reading {toml_file}")
@@ -116,26 +121,35 @@ def common_md():
                 ops_with_docs.add(key)
                 if "aliases" in ops[key]:
                     ops_with_docs |= set(ops[key]["aliases"])
+                if "description" in ops[key]:
+                    render = pdf_op_extended_template.render(name=key, **ops[key])
+                    extended.append((key, render))
 
-            output += op_list_template.render(ops=ops.values())
-            output += "\n"
-            output += "\n".join([e[1] for e in extended]) + "\n\n"
+            pdf_ops_section += pdf_op_table_template.render(ops=ops.values())
+            pdf_ops_section += "\n"
+            pdf_ops_section += "\n".join([e[1] for e in extended]) + "\n\n"
 
-    output += Path(DOCS_DIR / "advanced.md").read_text() + "\n\n"
+            html_ops_section += html_op_list_template.render(ops=ops.values())
 
-    output += "\\appendix\n\n"
+    advanced = Path(DOCS_DIR / "advanced.md").read_text() + "\n\n"
 
-    output += "# Alphabetical list of OPs and MODs\n\n"
+    pdf_alpha_ops = "\\appendix\n\n"
+    pdf_alpha_ops += "# Alphabetical list of OPs and MODs\n\n"
+    html_alpha_ops = pdf_alpha_ops
+
     sorted_ops = [kv[1] for kv in sorted(all_ops_dict.items())]
-    output += op_table_template.render(ops=sorted_ops)
+    pdf_alpha_ops += pdf_op_table_template.render(ops=sorted_ops)
+    html_alpha_ops += html_op_table_template.render(ops=sorted_ops)
 
-    output += "\n\n# Missing documentation\n\n"
-    missing_ops = all_ops - ops_with_docs
-    output += ", ".join([f"`{o}`" for o in sorted(missing_ops)]) + "\n\n"
+    missing = "\n\n# Missing documentation\n\n"
+    missing += ", ".join([f"`{o}`" for o in sorted(all_ops - ops_with_docs)]) + "\n\n"
 
-    output += Path(ROOT_DIR / "CHANGELOG.md").read_text() + "\n\n"
+    changelog = Path(ROOT_DIR / "CHANGELOG.md").read_text() + "\n\n"
 
-    return output
+    pdf_output = intro + pdf_ops_section + advanced + pdf_alpha_ops + missing + changelog
+    html_output = intro + html_ops_section + advanced + html_alpha_ops + missing + changelog
+
+    return {"pdf": pdf_output, "html": html_output}
 
 
 def main():
@@ -143,7 +157,9 @@ def main():
         sys.exit("Please supply a filename")
 
     input_format = "markdown"
-    output = common_md()
+    pdf_output = common_md()
+    html_output = pdf_output["html"]
+    pdf_output = pdf_output["pdf"]
 
     print()
 
@@ -154,11 +170,11 @@ def main():
         ext = p.suffix
 
         if ext == ".md":
-            p.write_text(output)
+            p.write_text(pdf_output)
         elif ext == ".html":
-            output = "# " + VERSION_STR + "\n\n" + output
+            html_output = "# " + VERSION_STR + "\n\n" + html_output
             pypandoc.convert_text(
-                output,
+                html_output,
                 format=input_format,
                 to="html5",
                 outputfile=str(p),
@@ -173,7 +189,7 @@ def main():
             latex_preamble = env.get_template("latex_preamble.jinja2.md")
             latex = latex_preamble \
                 .render(title=VERSION_STR, fonts_dir=FONTS_DIR) + "\n\n"
-            latex += output
+            latex += pdf_output
             pandoc_version = int(pypandoc.get_pandoc_version()[0])
             engine = ("--pdf-engine=xelatex"
                       if pandoc_version >= 2
---
+++ utils/templates/pdf_op_extended.jinja2.md
@@ -0,0 +1,20 @@
+### `{{ name }}`
+
+{% if prototype_set is defined %}
+ - **`{{ prototype }}`** / **`{{ prototype_set }}`**
+{% else %}
+ - **`{{ prototype }}`**
+{% endif %}
+{% if aliases is defined %}
+{% for alias in aliases %}
+ - _alias_: **`{{ alias }}`**
+{% endfor %}
+{% endif %}
+
+{% if description is defined %}
+{{ description }}
+{% else %}
+{% if short is defined %}
+{{ short }}
+{% endif %}
+{% endif %}
